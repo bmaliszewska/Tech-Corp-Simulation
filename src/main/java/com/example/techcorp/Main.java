26507a78d640c8cd2aad04510fd20321941e9926
bmaliszewska <bmaliszewska83@gmail.com>
2026-03-14 21:39:56 +0100

    Update Main.java

--- src/main/java/com/example/techcorp/Main.java
+++ src/main/java/com/example/techcorp/Main.java
@@ -8,14 +8,17 @@ public class Main {
 
         Employee anna = new Employee("Anna", 8, 7000);
         Employee piotr = new Employee("Piotr", 6, 6500);
+        Employee kasia = new Employee("Kasia", 2, 3333);
 
         company.hire(anna);
         company.hire(piotr);
+        company.hire(kasia);
 
         Project project = new Project("Mobile␣App", 30);
 
         project.addEmployee(anna);
         project.addEmployee(piotr);
+        project.addEmployee(kasia);
 
         company.startProject(project);
 
